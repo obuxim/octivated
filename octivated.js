5e48bfc5df855c67300f03364fcebb334bff4a08
Zubair Hasan Zubo <zubo@octoriz.com>
2020-05-10 02:24:56 +0600

    v1

---
+++ octivated.js
@@ -0,0 +1,20 @@
+/*
+  Plugin name:      Octivated
+  Author:           Zubair Hasan Zubo
+  Contact Author:   https://fb.me/obuxim
+  Company:          Octoriz Private Limited
+  Contact Company:  https://octoriz.com
+*/
+
+
+let links = document.getElementsByTagName('a')  // Gets all the links in a document
+let activePath = window.location.href  // Checks the current URL & URI
+// Loop through all the links of documents to check which one is active
+for (i=0; i<links.length; i++){
+    let link = links[i]
+    let linkPath = link.href
+    if(activePath == linkPath){
+        link.classList.add('active')  // Adds .active class to the active link.
+        console.log('Octivated! - https://github.com/obuxim/octivated')
+    }
+}
