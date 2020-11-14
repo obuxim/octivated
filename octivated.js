347c0f64c07606a5810563f0f24f5a521908e832
Zubair Hasan Zubo <obuxim@gmail.com>
2020-11-15 00:03:50 +0600

    Event Listener Added as Fix

--- octivated.js
+++ octivated.js
@@ -2,19 +2,21 @@
   Plugin name:      Octivated
   Author:           Zubair Hasan Zubo
   Contact Author:   https://fb.me/obuxim
-  Company:          Octoriz Private Limited
-  Contact Company:  https://octoriz.com
+  Company:          Duoneos
+  Contact Company:  https://duoneos.com
 */
 
 
-let links = document.getElementsByTagName('a')  // Gets all the links in a document
-let activePath = window.location.href  // Checks the current URL & URI
-// Loop through all the links of documents to check which one is active
-for (i=0; i<links.length; i++){
-    let link = links[i]
-    let linkPath = link.href
-    if(activePath == linkPath){
-        link.classList.add('active')  // Adds .active class to the active link.
-        console.log('Octivated! - https://github.com/obuxim/octivated')
-    }
-}
+document.addEventListener("DOMContentLoaded", function(event) { 
+  let links = document.getElementsByTagName('a')  // Gets all the links in a document
+  let activePath = window.location.href  // Checks the current URL & URI
+  // Loop through all the links of documents to check which one is active
+  for (i=0; i<links.length; i++){
+      let link = links[i]
+      let linkPath = link.href
+      if(activePath == linkPath){
+          link.classList.add('active')  // Adds .active class to the active link.
+          console.log('Octivated! - https://github.com/obuxim/octivated')
+      }
+  }
+});
